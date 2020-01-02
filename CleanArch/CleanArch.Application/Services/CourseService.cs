df93c0f79f5cf7d68e5a60ee41522dd635940131
Tulio <tuliomendelevio@outlook.com>
2020-01-02 16:50:22 +0100

    Commit implementation of Service and Repository GetCourses

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly ICourseRepository _repository;
+
+        public CourseService(ICourseRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel
+            {
+                Courses = _repository.GetCourses()
+            };
+
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseReporitory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseReporitory : ICourseRepository
+    {
+        private readonly UniversityDbContext _dbContext;
+
+        public CourseReporitory(UniversityDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _dbContext.Courses;
+        }
+    }
+}
