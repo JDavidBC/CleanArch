4399011bfd3804c9df09519ea564a6c7ca3235cf
Tulio <tuliomendelevio@outlook.com>
2020-01-02 17:57:16 +0100

    Changes in presentation layer

--- CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArch.Mvc.Controllers
 {
     public class CourseController : Controller
     {
+        private readonly ICourseService _service;
+
+        public CourseController(ICourseService service)
+        {
+            _service = service;
+        }
+        
         public IActionResult Index()
         {
-            return View();
+            var model = _service.GetCourses();
+            
+            return View(model);
         }
     }
 }
