cdfe76b11e7d8ee20dc3e2d302784d3048c5a2f9
Tulio <tuliomendelevio@outlook.com>
2020-01-02 17:18:39 +0100

    Commit of Ioc Layer and mod Startup for dependency container.

---
+++ CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            
+        }
+    }
+}
